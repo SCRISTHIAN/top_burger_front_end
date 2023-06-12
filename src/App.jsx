0a717c6711ff443067b171691e9920202a3f6beb
Cristhian-L-Q-N <quispeninacristhianlucio@gmail.com>
2023-06-12 05:09:14 -0400

    s-cristhian

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
-import Login from "./components/Login.jsx";
-import Platos from "./views/Platos.jsx";
-import NavBar from "./views/NavBar.jsx";
-import Reportes from "./views/Reportes";
-import SearchBarContainer from "./views/SearchBarContainer";
-import NotFound from "./views/NotFound";
-
+import { Route, Routes, useLocation } from "react-router-dom";
+import Login from "./views/Login/Login.jsx";
+import Platos from "./views/Platos/Platos.jsx";
+import NavBar from "./components/NavBar/NavBar.jsx";
+import {Reportes} from "./views/Reportes/index.js";
+import {SearchBarContainer} from "./components/SearchBarContainer/index.js";
+import {NotFound} from "./views/NotFound/index.js";
+import Ingredientes from "./views/Ingredientes/Ingredientes";
+import Ordenes from "./views/Ordenes/Ordenes";
+import MenuDelDia from "./views/MenuDelDia/MenuDelDia";
+import { Provider } from "react-redux";
+import { Proveedores } from "./views/Proveedores";
+import { store } from "./redux/index.js";
+import Dashboard from "./views/Dashboard/Dashboard";
+import Modelos from "./views/Modelos/Modelos";
 function App() {
   const location = useLocation();
   const notLoginPage = location.pathname !== "/login";
-
   return (
     <>
       <div className="app-container">
-        {notLoginPage && <NavBar />}
+      {notLoginPage && <NavBar />}
         <div className="app-content">
           {notLoginPage && <SearchBarContainer />}
+          <Provider store={store}>
           <Routes>
             <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element />
-            <Route path="/menudeldia" element />
+            <Route path="/dashboard" element={<Dashboard/>}/>
+            {/* <Route path="/menudeldia" element={<MenuDelDia/>} />  */}
             <Route path="/platos" element={<Platos />} />
-            <Route path="/ingredientes" element />
+            <Route path="/ingredientes" element= {<Ingredientes />} /> 
             <Route path="/reportes" element={<Reportes />} />
-            <Route path="/proveedores" element />
-            <Route path="/ordenes" element />
+            <Route path="/proveedores" element={<Proveedores/>} />
+            {/* <Route path="/ordenes" element={<Ordenes/>} /> */}
+            <Route path="/modelos" element={<Modelos/>} /> 
             <Route path="*" element={<NotFound />} />
           </Routes>
+          </Provider>
         </div>
       </div>
     </>
